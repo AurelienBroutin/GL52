a9352eb7bd193355498479799a9fb908727f4dfe
marinecollet <marine.collet10@gmail.com>
2018-06-24 21:55:21 +0200

    Ajout elements interface

--- projectGL52/src/view/VehiculeOverviewController.java
+++ projectGL52/src/view/VehiculeOverviewController.java
@@ -8,6 +8,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.util.Callback;
@@ -16,6 +17,7 @@ import model.Vehicule;
 public class VehiculeOverviewController {
 	 @FXML
 	private ListView<Vehicule> VehiculeList;
+		
 
 	MainApp mainApp;
 
